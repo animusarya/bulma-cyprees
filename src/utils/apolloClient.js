cc83394612ce8862a7cf1b7588dc8f3859bffb96
Parminder Klair <perminder.klair@gmail.com>
2019-06-21 16:03:41 +0530

    added easy peasy library

--- src/utils/apolloClient.js
+++ src/utils/apolloClient.js
@@ -1,14 +1,12 @@
 import ApolloClient from 'apollo-client';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { CachePersistor } from 'apollo-cache-persist';
-import { withClientState } from 'apollo-link-state';
 import { onError } from 'apollo-link-error';
 import { ApolloLink } from 'apollo-link';
 import { setContext } from 'apollo-link-context';
 import { createHttpLink } from 'apollo-link-http';
 
 import config from './config';
-import { defaults, resolvers, typeDefs } from '../store';
 
 const cache = new InMemoryCache();
 
@@ -44,12 +42,6 @@ const client = new ApolloClient({
       if (config.debug) {
         console.log('onError', graphQLErrors, networkError);
       }
-    }),
-    withClientState({
-      resolvers,
-      cache,
-      defaults: window.localStorage.length === 0 ? defaults : undefined,
-      typeDefs
     }),
     authLink.concat(httpLink)
   ]),
