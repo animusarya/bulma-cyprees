9db176186f33b0334b6739ebc3493d93d716f627
gagansaini1212 <gagansaini1212@gmail.com>
2019-07-12 16:53:35 +0530

    completed set password page

--- src/pages/SetPassword.js
+++ src/pages/SetPassword.js
@@ -22,10 +22,19 @@ const setPasswordMutation = gql`
   }
 `;
 
-const FormContainer = styled.div`
-  margin-top: 10rem;
-  padding: 0 3rem;
+const Container = styled.div`
+  .column:last-child {
+    align-self: center;
+  }
 `;
+
+const FormContainer = styled.div`
+  padding: 0 3rem;
+  @media only screen and (max-width: 768px) {
+    padding: 2rem;
+  }
+`;
+
 const Logo = styled.img`
   width: 300px;
   height: auto;
@@ -38,7 +47,7 @@ const SetPassword = () => {
   return (
     <Layout>
       <Seo title="SetPassword" description="Some description here." />
-      <div className="columns">
+      <Container className="columns">
         <div className="column">
           <img src={loginBg} alt="SetPassword banner" />
         </div>
@@ -50,7 +59,7 @@ const SetPassword = () => {
             {res.fetching ? <Loading /> : null}
           </FormContainer>
         </div>
-      </div>
+      </Container>
       <Footer />
     </Layout>
   );
