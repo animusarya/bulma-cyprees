760d3bc0c0e153bb7a88cb45a40430e6bf3f630a
Parminder Klair <perminder.klair@gmail.com>
2019-10-27 11:13:09 +0530

    updated api url to correct

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,8 +1,8 @@
 export default {
   debug: process.env.NODE_ENV === 'development',
   siteName: 'Intellishare',
-  graphQlUri: 'https://api.intellishare.online/',
-  // graphQlUriDev: 'https://api.intellishare.online/',
+  graphQlUri: 'https://api.intellishare.online/api',
+  // graphQlUriDev: 'https://api.intellishare.online/api',
   graphQlUriDev: 'http://localhost:4000',
   projectKey: 'intellishare',
   stripePublishableKey: 'pk_test_mjhAjr7ARWNkUU2Yj6IQrjxb',
