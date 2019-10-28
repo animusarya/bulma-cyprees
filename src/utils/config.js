c50de35f8b43c9c2900b0457f20fd4e55a509f89
Parminder Klair <perminder.klair@gmail.com>
2019-10-28 14:35:57 +0530

    local uri fix

--- src/utils/config.js
+++ src/utils/config.js
@@ -3,7 +3,7 @@ export default {
   siteName: 'Intellishare',
   graphQlUri: 'https://api.intellishare.online/api',
   // graphQlUriDev: 'https://api.intellishare.online/api',
-  graphQlUriDev: 'http://localhost:4000',
+  graphQlUriDev: 'http://localhost:4000/api',
   projectKey: 'intellishare',
   stripePublishableKey: 'pk_test_mjhAjr7ARWNkUU2Yj6IQrjxb',
   currency: '£',
