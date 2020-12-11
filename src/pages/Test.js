aef3c2314bdeec44ab6f12b4206ab5de225d6d4b
Parminder Klair <perminder.klair@gmail.com>
2020-12-11 12:10:30 +0530

    fix

--- src/pages/Test.js
+++ src/pages/Test.js
@@ -3,19 +3,12 @@ import React from 'react';
 import Layout from '../components/Layout';
 import Seo from '../components/Seo';
 
-import emailLogo from '../assets/images/email-logo.png';
-import emailBanner from '../assets/images/email-banner.png';
-
 const Test = () => {
   return (
     <Layout>
       <Seo title="Test" description="Some description here." />
       <div className="container">
         <h1>Test page</h1>
-        <div>
-          <img src={emailLogo} alt="email logo" />
-        </div>
-        <img src={emailBanner} alt="email banner" />
       </div>
     </Layout>
   );
