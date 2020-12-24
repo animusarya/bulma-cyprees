6c37fb87a087ac88d4d60d44ba8d5ecb61e97bd2
Parminder Klair <perminder.klair@gmail.com>
2020-12-24 13:13:33 +0530

    api endpoint updated

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,8 +1,8 @@
 export default {
   debug: process.env.NODE_ENV === 'development',
   siteName: 'Reviews System',
-  graphQlUri: 'https://api.intellishare.online/api',
-  graphQlUriDev: 'https://api.intellishare.online/api',
+  graphQlUri: 'https://reviews-system-api.vercel.app/api',
+  graphQlUriDev: 'https://reviews-system-api.vercel.app/api',
   // graphQlUriDev: 'http://localhost:4000/api',
   projectKey: 'reviews-system-app',
   currency: '£',
