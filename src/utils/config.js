5a35d0bf0a1bfbe8830a1fe0eaf6486c31640e30
gagansaini1212 <gagansaini1212@gmail.com>
2021-03-15 15:09:07 +0530

    uodate url

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,7 +1,7 @@
 export default {
   debug: process.env.NODE_ENV === 'development',
   siteName: 'Reviews System',
-  websiteUrl: 'https://web.reviewourservice.com',
+  websiteUrl: 'https://web.xyz.com',
   graphQlUri: 'https://reviews-system-api.vercel.app/api',
   graphQlUriDev: 'https://reviews-system-api.vercel.app/api',
   // graphQlUriDev: 'http://localhost:4000/api',
