8241d6faf153234abe21a31839c6d089368e89dd
dharmveer97 <dvbangar8@gmail.com>
2021-04-02 02:07:55 +0530

    fixed

--- src/components/sidebar/SubMenu.js
+++ src/components/sidebar/SubMenu.js
@@ -31,24 +31,22 @@ const SubMenu = ({ item }) => {
   return (
     <>
       <SidebarLink to={item.path} onClick={item.subNav && showSubNav}>
-        <div className="has-text-primary-light">
+        <div className="has-text-primary-light is-align-items-center is-flex">
           {item.icon}
           <SidebarLabel className="has-text-primary-light">
             {item.title}
           </SidebarLabel>
         </div>
-        <div>
-          {item.subNav && subNav ? (
-            <RiIcons.RiArrowDownSFill />
-          ) : item.subNav ? (
-            <RiIcons.RiArrowRightSFill />
-          ) : null}
-        </div>
+        {item.subNav && subNav ? (
+          <RiIcons.RiArrowDownSFill />
+        ) : item.subNav ? (
+          <RiIcons.RiArrowRightSFill />
+        ) : null}
       </SidebarLink>
       {subNav &&
         item.subNav.map((item1) => (
           <DropdownLink
-            className="has-text-primary-light"
+            className="has-text-primary-light child pl-6"
             to={item1.path}
             key={item1.title}>
             <SidebarLabel>{item1.title}</SidebarLabel>
