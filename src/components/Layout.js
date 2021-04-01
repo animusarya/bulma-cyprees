b259768bd310f6498b08caea4175f7b346baec32
dharmveer97 <dvbangar8@gmail.com>
2021-04-01 16:08:28 +0530

    remove extra components

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -4,9 +4,7 @@ import styled from 'styled-components';
 
 import config from '../utils/config';
 import Footer from './Footer';
-import Header from './Header';
 import Sidebar from './Sidebar';
-import MainColumn from './MainColumn';
 
 const Container = styled.div``;
 
@@ -14,17 +12,14 @@ const Wrapper = styled.div``;
 const Layout = ({ children, showFooter }) => (
   <Container>
     <Helmet title={config.siteName} />
-    <Header />
 
     <Wrapper className="columns">
-      <div className="column px-0 left-column is-2">
+      <div className="column is-2 px-0 left-column">
         <Sidebar />
       </div>
       <div className="column is-10 right-column">
-        <MainColumn>
-          {children}
-          {showFooter && <Footer />}
-        </MainColumn>
+        {children}
+        {showFooter && <Footer />}
       </div>
     </Wrapper>
   </Container>
