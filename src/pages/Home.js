33ae16d7f70cdd8d2bb49d829f9b91515441b1d5
iamkunall <103kunalsharma@gmail.com>
2021-04-07 16:52:57 +0530

    login redirect fix

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,20 +1,26 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Redirect } from 'react-router-dom';
 
 import Seo from '../components/Seo';
 
 const Home = () => (
   <>
     <Seo title="Home" description="Some description here." />
-    <section className="section">
-      <div className="container">
-        <Link to="/login">Login</Link>
-        <br />
-        <Link to="/register">Register</Link>
-        <br />
-        <Link to="/forgot-password">Forgot Password</Link>
-      </div>
-    </section>
+    {/* <section className="section">
+        <div className="container">
+          <Link to="/login">Login</Link>
+          <br />
+          <Link to="/register">Register</Link>
+          <br />
+          <Link to="/forgot-password">Forgot Password</Link>
+        </div>
+      </section> */}
+    <Redirect
+      to={{
+        pathname: '/login',
+        // state: { from: location },
+      }}
+    />
   </>
 );
 
