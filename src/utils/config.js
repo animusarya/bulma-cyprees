14696c65cede39e07edf572e8b80070991700889
Parminder Klair <perminder.klair@gmail.com>
2021-04-21 14:24:12 +0530

    Update config.js

--- src/utils/config.js
+++ src/utils/config.js
@@ -4,7 +4,7 @@ export default {
   websiteUrl: 'https://web.xyz.com',
   graphQlUri: 'https://rdglazing-api.vercel.app/api/graphql',
   graphQlUriDev: 'https://rdglazing-api.vercel.app/api/graphql',
-  // graphQlUriDev: 'http://localhost:4000/api',
+  // graphQlUriDev: 'http://localhost:4000/api/graphql',
   projectKey: 'rd-glazing-app',
   currency: '£',
 };
