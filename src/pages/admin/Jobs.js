57c6cec29b4e7fd5e1f9613c71d2339a291926fd
dharmveer97 <dvbangar8@gmail.com>
2021-04-28 14:06:01 +0530

    fixed

--- src/pages/admin/Jobs.js
+++ src/pages/admin/Jobs.js
@@ -1,117 +1,90 @@
-import React, { useState } from 'react';
+import React, { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 
-import Fuse from 'fuse.js';
+import { useQuery } from '@apollo/client';
+import gql from 'graphql-tag';
+import Swal from 'sweetalert2';
+
+// import Fuse from 'fuse.js';
 
 import Seo from '../../components/Seo';
 import Layout from '../../components/Layout';
 import { JobTable } from '../../components/jobs';
 import DashboardMenu from '../../components/global/DashboardMenu';
-// import useSearchFilters from '../../hooks/useSearchFilter';
+import { Loading, EmptyState } from '../../components/elements';
 
-const tableData = [
-  {
-    jobNumber: 12001,
-    site: 'Stables',
-    dueDate: '29/09/20',
-    assigned: 'Obama',
-    status: true,
-  },
-  {
-    jobNumber: 12002,
-    site: 'Tables',
-    dueDate: '29/09/20',
-    assigned: 'Dan',
-    status: true,
-  },
-  {
-    jobNumber: 12003,
-    site: 'Anger Menu',
-    dueDate: '29/09/20',
-    assigned: 'Julia',
-    status: true,
-  },
-  {
-    jobNumber: 12004,
-    site: 'Smart Stables',
-    dueDate: '29/09/20',
-    assigned: 'Richard',
-    status: true,
-  },
-  {
-    jobNumber: 12005,
-    site: 'Micky',
-    dueDate: '29/09/20',
-    assigned: 'Mark',
-    status: true,
-  },
-  {
-    jobNumber: 12006,
-    site: 'The Stables',
-    dueDate: '9/09/20',
-    assigned: 'Steve',
-    status: true,
-  },
-  {
-    jobNumber: 12007,
-    site: 'The Stables',
-    dueDate: '29/09/20',
-    assigned: 'Kel',
-    status: true,
-  },
-  {
-    jobNumber: 12008,
-    site: 'The Stables',
-    dueDate: '29/09/20',
-    assigned: 'Maxwell',
-    status: true,
-  },
-  {
-    jobNumber: 12009,
-    site: 'The Stables',
-    dueDate: '19/09/20',
-    assigned: 'Harry',
-    status: true,
-  },
-];
+const allJobsQuery = gql`
+  query allJobs($filters: JobFilters) {
+    allJobs(filters: $filters) {
+      id
+      code
+      dueDate
+      status
+      customer {
+        id
+        name
+      }
+    }
+  }
+`;
 
 const Jobs = () => {
   const { status } = useParams();
+
   const heading = status === 'revisit' ? 'Jobs that need Revisit' : 'Jobs';
 
-  const [query, setUpdateQuery] = useState('');
-  const fuse = new Fuse(tableData, {
-    keys: ['jobNumber', 'site', 'dueDate', 'assigned'],
-    includeScore: true,
+  // const [query, setUpdateQuery] = useState('');
+  // const fuse = new Fuse(tableData, {
+  //   keys: ['jobNumber', 'site', 'dueDate', 'assigned'],
+  //   includeScore: true,
+  // });
+
+  // const results = fuse.search(query);
+
+  // const allJobs = query
+  //   ? results.map((character) => character.item)
+  //   : tableData;
+
+  // const onSearch = ({ currentTarget }) => {
+  //   setUpdateQuery(currentTarget.value);
+  // };
+  const { data, error, loading } = useQuery(allJobsQuery, {
+    fetchPolicy: 'cache-and-network',
+    variables: {
+      filters: {
+        status,
+      },
+    },
   });
 
-  const results = fuse.search(query);
+  useEffect(() => {
+    if (error)
+      Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: error.message,
+      });
+  }, [error]);
 
-  const characterResults = query
-    ? results.map((character) => character.item)
-    : tableData;
-
-  const onSearch = ({ currentTarget }) => {
-    setUpdateQuery(currentTarget.value);
-  };
-
-  // const [allData, query, { onSearch, setAllData }] = useSearchFilters();
-
-  // useEffect(() => {
-  //   if (tableData) setAllData(tableData);
-  // }, [tableData]);
+  const allJobs = data && data.allJobs ? data.allJobs : {};
 
   return (
     <Layout>
       <Seo title="Job page" description="View All Jobs" />
 
       <DashboardMenu
-        value={query}
+        // value={query}
         // onChange={onSearch}
-        onChange={(filters) => onSearch(filters, tableData)}
+        // onChange={(filters) => onSearch(filters, tableData)}
         hasSearchMenu
         heading={heading}>
-        <JobTable tableData={characterResults} status={status} />
+        {loading && !data && <Loading />}
+        <div className="has-text-centered mb-5">
+          {allJobs.length === 0 && !loading && <EmptyState />}
+        </div>
+        {allJobs && allJobs.length > 0 && (
+          <JobTable tableData={allJobs} status={status} />
+        )}
       </DashboardMenu>
     </Layout>
   );
