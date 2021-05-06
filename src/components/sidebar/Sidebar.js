653dcea0f122550c7d9075daf74567faeeb945f2
dharmveer97 <dvbangar8@gmail.com>
2021-05-06 12:16:24 +0530

    fixed

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
+import { useStoreActions } from 'easy-peasy';
 
 import * as FaIcons from 'react-icons/fa';
 import * as AiIcons from 'react-icons/ai';
+import * as BiIcons from 'react-icons/bi';
 
 import { IconContext } from 'react-icons/lib';
 import Logo from '../../assets/images/logo.png';
@@ -41,6 +43,18 @@ const SidebarNav = styled.nav`
 const ImageWrapper = styled.figure`
   width: auto !important;
   padding: 1.2rem;
+`;
+
+const LogoutWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 20px;
+  font-size: ${(props) => props.theme.fontSize};
+`;
+
+const SidebarLabel = styled.span`
+  margin-left: 16px;
 `;
 
 const sidebarData = [
@@ -103,24 +117,35 @@ const sidebarData = [
   },
 ];
 
-const Sidebar = () => (
-  <IconContext.Provider value={{ color: '#fff' }}>
-    <Nav className="is-hidden-desktop">
-      <NavIcon className="has-text-black" to="#">
-        <FaIcons.FaBars />
-      </NavIcon>
-    </Nav>
-    <SidebarNav>
-      <div>
-        <ImageWrapper className="is-hidden-mobile">
-          <img src={Logo} alt="logo" />
-        </ImageWrapper>
-        {sidebarData.map((item) => (
-          <SubMenu item={item} key={item.title} />
-        ))}
-      </div>
-    </SidebarNav>
-  </IconContext.Provider>
-);
+const Sidebar = () => {
+  const toggleLoggedIn = useStoreActions(
+    (actions) => actions.isLoggedIn.toggle,
+  );
 
+  return (
+    <IconContext.Provider value={{ color: '#fff' }}>
+      <Nav className="is-hidden-desktop">
+        <NavIcon className="has-text-black" to="#">
+          <FaIcons.FaBars />
+        </NavIcon>
+      </Nav>
+      <SidebarNav>
+        <div>
+          <ImageWrapper className="is-hidden-mobile">
+            <img src={Logo} alt="logo" />
+          </ImageWrapper>
+          {sidebarData.map((item) => (
+            <SubMenu item={item} key={item.title} />
+          ))}
+          <LogoutWrapper onClick={() => toggleLoggedIn(false)} className="ml-5">
+            <div className="has-text-white is-align-items-center is-flex">
+              <BiIcons.BiLogOutCircle />
+              <SidebarLabel className="has-text-white">Logout</SidebarLabel>
+            </div>
+          </LogoutWrapper>
+        </div>
+      </SidebarNav>
+    </IconContext.Provider>
+  );
+};
 export default Sidebar;
