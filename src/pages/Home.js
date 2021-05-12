e1779223a944d583cf41c52a0803f2b103a0a7ac
dharmveer97 <dvbangar8@gmail.com>
2021-05-12 17:46:23 +0530

    fixed

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,7 +5,34 @@ import Seo from '../components/Seo';
 const Home = () => (
   <>
     <Seo title="Home" description="Some description here." />
-    <p>hello my name is dharm</p>
+    <section className="section">
+      <div className="container">
+        <form>
+          <div>
+            <label className="label">Name</label>
+            <input className="input" required name="name" id="email" />
+          </div>
+          <div>
+            <label className="label">Email</label>
+            <input className="input" required name="email" id="email" />
+          </div>
+          <div>
+            <label className="label">Your message</label>
+            <textarea
+              className="textarea"
+              id="message"
+              name="message"
+              required
+            />
+          </div>
+          <div>
+            <button className="button" type="submit">
+              SEND
+            </button>
+          </div>
+        </form>
+      </div>{' '}
+    </section>
   </>
 );
 
